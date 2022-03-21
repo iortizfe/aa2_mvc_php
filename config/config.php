35c6b30a0e2fbae3d93cd3e16765956752aa42a1
iortizfe <iortizfe@uoc.edu>
2022-03-21 05:46:59 +0100

    init

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+  //VARIABLES BBDD
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'aula_mvc3');
+	
+  // VARIABLES ROOT 
+  define('APPROOT', dirname( dirname(__FILE__)));
+  define('URLROOT', 'http://localhost/aa2_mvc_php');
+
+  // VARIABLES NOMBRE
+  define('APPNAME', 'AULA MVC3 P2');
