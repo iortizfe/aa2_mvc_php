2264565832ce49f4e2b6dd5eee19779769cfdad5
iortizfe <iortizfe@uoc.edu>
2022-04-09 10:23:55 +0200

    login register 1.9

--- libraries/MiddlewareRoute.php
+++ libraries/MiddlewareRoute.php
@@ -1,6 +1,6 @@
 <?php
 if (!isset($_SESSION['user']) && $route_type == "private") {
-    header('Location: '.URLROOT.'controllers/Login.php');
+    header('Location: '.URLROOT.'controllers/login.php');
 }else if(isset($_SESSION['user']) && $route_type == "not-private") {
     header('Location: '.URLROOT.'/');
 }else if(isset($_SESSION['user']) && $_SESSION['role'] != 'admin' && $route_type == "admin") {
